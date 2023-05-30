86af44d1ac12a0f1bc0f920f0b2a132666acbca7
lolenseu <lolenseu.koleutejeu248@gmail.com>
2023-05-30 14:55:38 +0800

    Update quadcopter/main.py

--- quadcopter/main.py
+++ quadcopter/main.py
@@ -1,4 +1,4 @@
-# [v0.1.3]
+# [v0.1.4]
 
 # -------------------- [Import from Library] --------------------
 #import utime
@@ -15,6 +15,38 @@ from machine import*
 
 # -------------------- [~Import from Modules] --------------------
 
+# -------------------- [Variables] --------------------
+# ---------- [MPU6050 Dvice on I2C] ----------
+# [MPU6050 address on I2C bus]
+mpu6050_addr=0x6B
+
+# [PWR_MGMT_1 memory address]
+mpu6050_pwr_mgmt_1=0x6B
+
+# [Accelerometer high and low register for each axis]
+mpu6050_accel_xaxis_high=0x3B
+mpu6050_accel_xaxis_low=0x3C
+mpu6050_accel_yaxis_high=0x3D
+mpu6050_accel_yaxis_low=0x3E
+mpu6050_accel_zaxis_high=0x3F
+mpu6050_accel_zaxis_low=0x40
+
+# [Gyroscope high and low register for each axis]
+mpu6050_gyro_xaxis_high=0x43
+mpu6050_gyro_xaxis_low=0x44
+mpu6050_gyro_yaxis_high=0x45
+mpu6050_gyro_yaxis_low=0x46
+mpu6050_gyro_zaxis_high=0x47
+mpu6050_gyro_zaxis_low=0x48
+
+# [Accelerometer's LSB/g (least significant bits per gravitational force) sensitivity]
+mpu6050_lsbg = 16384.0
+
+# [Gyroscope's LSB/g sensitivity]
+mpu6050_lsbds = 131.0 
+# ---------- [~MPU6050 Dvice on I2C] ----------
+# -------------------- [~Variables] --------------------
+
 # -------------------- [Configuration File] --------------------
 
 
@@ -22,9 +54,10 @@ from machine import*
 # -------------------- [~Configuration File] --------------------
 
 # -------------------- [Functions] --------------------
+# --------------- [MPU6050 Device Functions] ---------------
 # [Set all bits in the PWR_MGMT_1 register to 0]
 def mpu6050_init(i2c):
-    i2c.writeto_mem(mpu6050_addr,mpu6050_pwr_mgmt_1, b'\x00')
+    i2c.writeto_mem(mpu6050_addr,mpu6050_pwr_mgmt_1,b'\x00')
 
 # [Combine the values of high and low]
 def combine_register_values(high,low):
@@ -34,12 +67,12 @@ def combine_register_values(high,low):
 
 # [Get Accelerometer values]
 def mpu6050_get_accel(i2c):
-    accel_xaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_xaxis_out_high,1)
-    accel_xaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_xaxis_out_low,1)
-    accel_yaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_yaxis_out_high,1)
-    accel_yaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_yaxis_out_low,1)
-    accel_zaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_zxis_out_high,1)
-    accel_zaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_acc_zxis_out_low,1)
+    accel_xaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_xaxis_high,1)
+    accel_xaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_xaxis_low,1)
+    accel_yaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_yaxis_high,1)
+    accel_yaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_yaxis_low,1)
+    accel_zaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_zaxis_high,1)
+    accel_zaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_accel_zaxis_low,1)
     
     return [combine_register_values(accel_xaxis_high,accel_xaxis_low)/mpu6050_lsbg,
             combine_register_values(accel_yaxis_high,accel_yaxis_low)/mpu6050_lsbg,
@@ -47,16 +80,17 @@ def mpu6050_get_accel(i2c):
 
 # [Get Gyroscope values]
 def mpu6050_get_gyro(i2c):
-    gyro_xaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_xaxis_out_high,1)
-    gyro_xaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_xaxis_out_low,1)
-    gyro_yaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_yaxis_out_high,1)
-    gyro_yaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_yaxis_out_low,1)
-    gyro_zaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_zxis_out_high,1)
-    gyro_zaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyr_zxis_out_low,1)
+    gyro_xaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_xaxis_high,1)
+    gyro_xaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_xaxis_low,1)
+    gyro_yaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_yaxis_high,1)
+    gyro_yaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_yaxis_low,1)
+    gyro_zaxis_high=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_zaxis_high,1)
+    gyro_zaxis_low=i2c.readfrom_mem(mpu6050_addr,mpu6050_gyro_zaxis_low,1)
     
     return [combine_register_values(gyro_xaxis_high,gyro_xaxis_low)/mpu6050_lsbds,
             combine_register_values(gyro_yaxis_high,gyro_yaxis_low)/mpu6050_lsbds,
             combine_register_values(gyro_zaxis_high,gyro_zaxis_low)/mpu6050_lsbds]
+# --------------- [~MPU6050 Device Functions] ---------------
 
 # --------------- [Shell Print] ---------------
 
@@ -64,41 +98,12 @@ def mpu6050_get_gyro(i2c):
 # --------------- [~Shell Print] ---------------
 # -------------------- [~Functions] --------------------
 
-# -------------------- [Variables] --------------------
-# ---------- [MPU6050 Dvice over I2C] ----------
-# [MPU6050 address on I2C bus]
-mpu6050_addr=0x6B
-
-# [PWR_MGMT_1 memory address]
-mpu6050_pwr_mgmt_1=0x6B
-
-# [Accelerometer high and low register for each axis]
-mpu6050_acc_xaxis_out_high=0x3B
-mpu6050_acc_xaxis_out_low=0x3C
-mpu6050_acc_yaxis_out_high=0x3D
-mpu6050_acc_yaxis_out_low=0x3E
-mpu6050_acc_zxis_out_high=0x3F
-mpu6050_acc_zxis_out_low=0x40
-
-# [Gyroscope high and low register for each axis]
-mpu6050_gyr_xaxis_out_high=0x43
-mpu6050_gyr_xaxis_out_low=0x44
-mpu6050_gyr_yaxis_out_high=0x45
-mpu6050_gyr_yaxis_out_low=0x46
-mpu6050_gyr_zxis_out_high=0x47
-mpu6050_gyr_zxis_out_low=0x48
-
-# [Accelerometer's LSB/g (least significant bits per gravitational force) sensitivity]
-mpu6050_lsbg = 16384.0
-
-# [Gyroscope's LSB/g sensitivity]
-mpu6050_lsbds = 131.0 
-# ---------- [~MPU6050 Dvice over I2C] ----------
-# -------------------- [~Variables] --------------------
-
 # -------------------- [Setup] --------------------
 # [Define I2C bus]
 i2c=I2C(0,sda=Pin(0),scl=Pin(1),freq=4000000)
+
+# [Initiate MPU6050 on I2C]
+mpu6050_init(i2c)
 # -------------------- [~Setup] --------------------
 
 # -------------------- [Loop] --------------------
